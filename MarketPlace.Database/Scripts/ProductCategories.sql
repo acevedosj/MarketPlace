0fdb776fab803892bf3516a7df1a70d9428bb78f
Jose Antonio Acevedo San Martin <acevedosj@msn.com>
2020-05-28 20:07:50 -0500

    organizacion script base de datos

---
+++ MarketPlace.Database/Scripts/ProductCategories.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO dbo.ProductCategories (ProductCategoryId, Code, Name)
+VALUES ('064A91CD-4581-45E4-A40E-BE6829B2557A', '02', 'Electrónica')
+GO
+
+INSERT INTO dbo.ProductCategories (ProductCategoryId, Code, Name)
+VALUES ('F12E3429-2A74-4624-BDE6-CF59725F160A', '01', 'Informática')
+GO
